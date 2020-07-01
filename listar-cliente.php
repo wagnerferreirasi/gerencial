8f1ea36d84cd17cdcb4a74c07224a7b7c16ea7bd
Wagner Ferreira <66226535+wagnerferreirasi@users.noreply.github.com>
2020-06-30 22:14:33 -0300

    auto-update vscode

---
+++ listar-cliente.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include_once('includes/mysql.php');
+?>
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Gerencial Ferreira S.I - Controle de Cliente e Cobranças</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="style/css/style.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+
+<body class="bg-light">
+<?php include_once('nav-bar.php'); ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+
+</html>
